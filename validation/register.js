b9bda529d3d5016fcd2b9877195bcc850ed03e87
Travis Cunningham <travis@enviral.io>
2019-04-23 17:44:47 -0700

    Delete User and misc bugs

--- validation/register.js
+++ validation/register.js
@@ -37,7 +37,7 @@ module.exports = function validateRegisterInput(data) {
     errors.password2 = "Confirm Password field is required";
   }
 
-  if (Validator.equals(data.password, data.password2)) {
+  if (!Validator.equals(data.password, data.password2)) {
     errors.password2 = "Passwords must match";
   }
 
