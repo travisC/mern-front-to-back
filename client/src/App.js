3ab98a5b5d608f310f60e0cdb2fc6b07695ae75c
Travis Cunningham <travis@enviral.io>
2019-06-22 13:28:33 -0700

    Adding post and comments

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import AddEducation from "./components/profile-forms/AddEducation";
 import Profiles from "./components/profiles/Profiles";
 import Profile from "./components/profile/Profile";
 import Posts from "./components/posts/Posts";
+import Post from "./components/post/Post";
 
 if (localStorage.token) {
   setAuthToken(localStorage.token);
@@ -44,6 +45,7 @@ const App = () => {
               <Route exact path="/profile/:id" component={Profile} />
               <PrivateRoute exact path="/dashboard" component={Dashboard} />
               <PrivateRoute exact path="/posts" component={Posts} />
+              <PrivateRoute exact path="/posts/:id" component={Post} />
               <PrivateRoute
                 exact
                 path="/create-profile"
